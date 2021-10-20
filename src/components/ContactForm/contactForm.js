425c0edd39a1913d10e86ef297dde3b67b9f68b3
MaryamShahvalizadeh <ShahvalizadehMaryam@gmail.com>
2021-10-20 10:21:09 +0330

    contactForm submit and handle change added.

--- src/components/ContactForm/contactForm.js
+++ src/components/ContactForm/contactForm.js
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import styles from "./contactForm.module.css";
 const ContactForm = () => {
+  const [formValue, setFormValue] = useState({
+    name: "",
+    email: "",
+  });
+  const inputChangeHandler = (e) => {
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formValue);
+    setFormValue({
+      name: "",
+      email: "",
+    });
+  };
   return (
     <div className={styles.contactFormPart}>
       <h3> Add contact</h3>
-      <form>
+      <form onSubmit={formSubmitHandler}>
         <div className={styles.formControl}>
           <label>Name</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="name"
+            value={formValue.name}
+            onChange={inputChangeHandler}
+          />
         </div>
         <div className={styles.formControl}>
           <label>Email</label>
-          <input type="email" />
+          <input
+            type="email"
+            name="email"
+            value={formValue.email}
+            onChange={inputChangeHandler}
+          />
         </div>
         <button>Add</button>
       </form>
