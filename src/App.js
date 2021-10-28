51f67b59983efe1895d63e31d3121135d92f81bc
MaryamShahvalizadeh <ShahvalizadehMaryam@gmail.com>
2021-10-28 10:14:57 +0330

    remove contactDetail part

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ContactForm from "./components/ContactForm/contactForm";
 import ContactList from "./components/ContactList/contactList";
 import HeaderComp from "./components/Header/header";
 import { Route } from "react-router-dom";
-import ContactDetail from "./components/ContactDetail/contactDetail";
 import EditContact from "./components/EditContact/editContact";
 import { ToastProvider } from "react-toast-notifications";
 const App = () => {
@@ -14,7 +13,6 @@ const App = () => {
       <Switch>
         <ToastProvider autoDismiss={true}>
           <Route path="/edit/:id" component={EditContact}></Route>
-          <Route path="/contacts/:id" component={ContactDetail}></Route>
           <Route path="/add" component={ContactForm}></Route>
           <Route path="/" exact component={ContactList}></Route>
         </ToastProvider>
